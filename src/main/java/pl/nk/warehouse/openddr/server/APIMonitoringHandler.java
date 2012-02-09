538dcad4e6f755ae961914df30e5e94bc8fc4a84
Wojciech Langiewicz <wlangiewicz@gmail.com>
2012-02-09 13:17:49 +0100

    fixed properties paths

---
+++ src/main/java/pl/nk/warehouse/openddr/server/APIMonitoringHandler.java
@@ -0,0 +1,24 @@
+package pl.nk.warehouse.openddr.server;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class APIMonitoringHandler implements HttpHandler {
+
+	public void handle(HttpExchange exchange) throws IOException {
+		String requestMethod = exchange.getRequestMethod();
+		if (requestMethod.equalsIgnoreCase("GET")) {
+			Headers responseHeaders = exchange.getResponseHeaders();
+			responseHeaders.set("Content-Type", "text/plain");
+			exchange.sendResponseHeaders(200, 0);
+			OutputStream responseBody = exchange.getResponseBody();
+			responseBody.write("OK".getBytes());
+			responseBody.close();
+		}
+	}
+
+}
